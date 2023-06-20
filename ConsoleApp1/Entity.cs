b582c3ba07687493ab9c792c70c6664257ec2882
Mahdi-Ostadrahim <84525990+Mahdi-Ostadrahim@users.noreply.github.com>
2023-06-20 14:35:00 +0330

    Initial commit

---
+++ ConsoleApp1/Entity.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Customer
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public int currency { get; private set; }
+
+        public Customer(string name, string email)
+        {
+            Name = name;
+            Email = email;
+            currency = 0;
+        }
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+        }
+        public void ChangeName(string name)
+        {
+            Name=name;
+        }
+        public void UpdateCurrency(int x)
+        {
+            currency += x;
+        }
+    }
+    public record Information
+    {
+        public int X { get; init; }
+        public int Y { get; init; }
+        public int Z { get; init; }
+        public float Rating { get; init; }
+    }
+    public class Product
+    {
+        public int UnitsInStock { get; private set; }
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public decimal Price { get; private set; }
+        public Information info { get; init; }
+
+        public Product(Information info, string name, decimal price, int units)
+        {
+            Name = name;
+            Price = price;
+            UnitsInStock = units;
+            this.info = info;
+        }
+        public void Buy(int x)
+        {
+            UnitsInStock += x;
+        }
+        public void Restock(int x)
+        {
+            UnitsInStock -= x;
+        }
+    }
+    public class Sender
+    {
+        public int ID { get; init; }
+        public string Name { get; init; }
+        public float Rating { get; private set; }
+        public string Carname { get; private set; }
+        public bool IsFree { get; private set; }
+        public Sender(int id, string name, string carname)
+        {
+            ID = id;
+            Name = name;
+            Carname = carname;
+            IsFree = true;
+            Rating = 0;
+        }
+        public void StartWork()
+        {
+            IsFree = false;
+        }
+        public void EndWork()
+        {
+            IsFree=true;
+        }
+    }
+}
---
+++ ConsoleApp1/Services.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Order
+    {
+        public int Id { get; init; }
+        public Product Product { get; init; }
+        public int count { get; private set; }
+        public Order(Product product, int id, int cnt=1)
+        {
+            Product = product;
+            Id = id;
+            count = Math.Min(cnt,Product.UnitsInStock);
+            Product.Buy(count);
+        }
+        public Order Update(int x)
+        {
+            if (Product.UnitsInStock < x)
+                return this;
+            Product.Buy(x - count);
+            return this;
+        }
+    }
+    public class Basket
+    {
+        public int ID { get; init; }
+        public Customer customer { get; init; }
+        public List<Order> total { get; private set; }
+        public Basket(Customer customer, Order order, int id)
+        {
+            this.customer = customer;
+            total = new List<Order>();
+            total.Add(order);
+            this.ID = id;
+        }
+        public void Add(Order ord)
+        {
+            total.Add(ord);
+        }
+        public void Delete(Order ord)
+        {
+            for (int i = 0; i < total.Count; i++)
+            {
+                if (total[i] == ord)
+                {
+                    total.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+        public void Update(Order order, int count)
+        {
+            if (count == 0)
+            {
+                Delete(order);
+                return;
+            }
+            for (int i = 0; i < total.Count; i++)
+                if (total[i] == order)
+                    total[i] = total[i].Update(count);
+            order.Update(count);
+            Add(order);
+        }
+    }
+    public class Send
+    {
+        public int ID { get; init; }
+        public Order order { get; init; }
+        public Sender sender { get; init; }
+        public string destination { get; init; }
+        public string currentLocation { get; private set; }
+        public DateTime starttime { get; init; }
+        public DateTime estimatedDeliveryTime { get; init; }
+        public Send(Order ord, Sender send, int id, string des, DateTime deliver)
+        {
+            sender = send;
+            sender.StartWork();
+            order = ord;
+            ID = id;
+            destination = des;
+            currentLocation = "";
+            starttime = DateTime.Now;
+            estimatedDeliveryTime = deliver;
+        }
+        public void UpdateLocation(string loc)
+        {
+            currentLocation = loc;
+        }
+        public string GetLocation()
+        {
+            return currentLocation;
+        }
+        public double RemainingPercent()
+        {
+            return ((DateTime.Now - starttime).TotalSeconds * 100) / ((estimatedDeliveryTime - starttime).TotalSeconds);
+        }
+    }
+}
